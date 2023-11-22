0d533fbb259eac9513d45e803b038ce8ac8cd169
soniikot <soniikot@gmail.com>
2023-11-21 16:15:19 -0800

    add draw part to function

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-
-
-function createScreen(){
-    for (i=1; i<257;i++){
-        if (i%16==0){
-           const container = document.querySelector('#container'); 
-            const block = document.createElement('div');
+const container = document.querySelector('#container'); 
+           
+function createScreen(grids){
+    for (i=1; i<grids*grids+1;i++){
+        if (i%grids==0){
+           
+const block = document.createElement('div');
+block.addEventListener('mouseenter',()=>{
+    block.style.backgroundColor ='black';
+})
+container.appendChild(block);
             block.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
           
-          block.classList.add('block');
-container.appendChild(block);
+
            
 const newLine = document.createElement('div');
            newLine.classList.add('break');
@@ -17,14 +20,35 @@ const newLine = document.createElement('div');
 
         }
         else{
-const block = document.createElement('div');
-block.classList.add('block');
-container.appendChild(block);
-block.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
+            
+        const block = document.createElement('div');
+        block.addEventListener('mouseenter',()=>{
+            block.style.backgroundColor ='black';
+        })
+        //block.classList.add('blocks');
+        container.appendChild(block);
+        block.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
+        }
+            
 
-    }}}
+
+    }}
 
 
-createScreen();
+createScreen(16);
+
+
+
+/*const blocks = document.querySelectionAll('block'); s
+blocks.forEach((block)=>{
+    block.addEventListener("mouseover", () => {
+block.style.backgroundColor ="blue";
+
+});
+});*/
+
+
     
+
+
    
